9b39c52bfef8900c8d1a82073988622756303b26
Infamous0192 <infamous0192@gmail.com>
2023-09-23 02:26:02 +0800

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,8 @@ Route::group(['middleware' => ['auth']], function () {
 
     Route::resource('user', App\Http\Controllers\UserController::class);
     Route::resource('mampu', App\Http\Controllers\MampuController::class);
+
+    Route::get('tpu/print', [App\Http\Controllers\PekerjaController::class, 'print'])->name('tpu.print');
     Route::resource('pekerja', App\Http\Controllers\PekerjaController::class);
 
     Route::get('keuangan', [App\Http\Controllers\KeuanganController::class, 'index'])->name('keuangan.index');
@@ -40,17 +42,17 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('makam/print', [App\Http\Controllers\MakamController::class, 'print'])->name('makam.print');
     Route::resource('makam', App\Http\Controllers\MakamController::class);
 
-    Route::resource('jenazah-kenal', App\Http\Controllers\JenazahKenalController::class);
     Route::get('jenazah-kenal/laporan', [App\Http\Controllers\JenazahKenalController::class, 'laporan'])->name('jenazah-kenal.laporan');
+    Route::resource('jenazah-kenal', App\Http\Controllers\JenazahKenalController::class);
 
-    Route::resource('jenazah', App\Http\Controllers\JenazahController::class);
     Route::get('jenazah/laporan', [App\Http\Controllers\JenazahController::class, 'laporan'])->name('jenazah.laporan');
+    Route::resource('jenazah', App\Http\Controllers\JenazahController::class);
 
 
+    Route::get('pewaris/laporan', [App\Http\Controllers\PewarisController::class, 'laporan'])->name('pewaris.laporan');
     Route::resource('pewaris', App\Http\Controllers\PewarisController::class)->parameters([
         'pewaris' => 'pewaris',
     ]);
-    Route::get('pewaris/laporan', [App\Http\Controllers\PewarisController::class, 'laporan'])->name('pewaris.laporan');
 
     Route::get('pesanan/print', [App\Http\Controllers\PesananController::class, 'print'])->name('pesanan.print');
     Route::resource('pesanan', App\Http\Controllers\PesananController::class);
